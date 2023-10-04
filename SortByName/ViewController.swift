3eb541880dff48558cd7d204acd25afc0b587eee
Nurtas2003 <n_serikkanov@kbtu.kz>
2023-10-04 21:50:54 +0600

    Initial Commit

---
+++ SortByName/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SortByName
+//
+//  Created by Nurtas Serikkanov on 04.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
